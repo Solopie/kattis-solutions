c3f7d94412629fca5d3dbc8321b9c4abc84d0548
Jesse Nguyen <jesse_nguyen9@yahoo.com>
2021-09-27 22:56:51 +1000

    oddmanout

--- java/oddmanout/Main.java
+++ java/oddmanout/Main.java
@@ -1,5 +1,24 @@
 import java.util.*;
 
-class Main {
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
 
+        for (int i = 1; i <= n; i++) {
+            HashMap<Integer, Integer> counts = new HashMap<>();
+            int g = sc.nextInt();
+            for (int j = 0; j < g; j++) {
+                int cur = sc.nextInt();
+                counts.put(cur, counts.getOrDefault(cur, 0) + 1);
+            }
+
+            for (Map.Entry<Integer, Integer> entry : counts.entrySet()) {
+                if (entry.getValue() == 1) {
+                    System.out.println("Case #" + i + ": " + entry.getKey());
+                }
+            }
+        }
+
+    }
 }
