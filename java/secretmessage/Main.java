de0f0b90a8b68e600c787736d3b834aba3f465ee
Jesse Nguyen <jesse_nguyen9@yahoo.com>
2021-10-08 16:46:39 +1100

    secretmessage

---
+++ java/secretmessage/Main.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        while (n-- > 0) {
+            String message = sc.next();
+
+            // Find next square number
+            int m = message.length();
+            while (Math.sqrt(m) * Math.sqrt(m) != m) {
+                m++;
+            }
+
+            int k = (int) Math.sqrt(m);
+
+            // int padding = m - message.length();
+            char[][] grid = new char[k][k];
+
+            int strIndex = 0;
+            for (int i = 0; i < grid.length; i++) {
+                for (int j = 0; j < grid[i].length; j++) {
+                    if (strIndex < message.length()) {
+                        grid[i][j] = message.charAt(strIndex);
+                        strIndex++;
+                    } else {
+                        grid[i][j] = '*';
+                    }
+                }
+            }
+
+            for (int col = 0; col < grid[0].length; col++) {
+                for (int row = grid.length - 1; row >= 0; row--) {
+                    if (grid[row][col] != '*') {
+                        System.out.print(grid[row][col]);
+                    }
+                }
+            }
+            System.out.println();
+        }
+    }
+}
