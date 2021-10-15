794b4443598ccdcdd3e2451f491a269a55cb4028
Jesse Nguyen <jesse_nguyen9@yahoo.com>
2021-10-16 10:38:32 +1100

    bijele

---
+++ java/bijele/Main.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.format("%d %d %d %d %d %d\n", 1 - sc.nextInt(), 1 - sc.nextInt(), 2 - sc.nextInt(), 2 - sc.nextInt(), 2 - sc.nextInt(), 8 - sc.nextInt());
+    }
+}
