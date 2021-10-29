a1d8cf012db44032127005da2b97bae6caa1fe44
Jesse Nguyen <jesse_nguyen9@yahoo.com>
2021-10-29 11:25:08 +1100

    jumbojavelin

---
+++ java/jumbojavelin/Main.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int total = 0;
+        for (int i = 0; i < n; i++) {
+            total += sc.nextInt();
+        }
+
+        System.out.println(total - (n - 1));
+
+    }
+}
