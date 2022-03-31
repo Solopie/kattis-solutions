ba3f50f564b8b60cd6b2cf1c62a8b8e2e8ce8f1a
Jesse Nguyen <jesse_nguyen9@yahoo.com>
2022-03-31 21:27:44 +1100

    simonsays

--- java/simonsays/Main.java
+++ java/simonsays/Main.java
@@ -1,25 +1,23 @@
 import java.util.*;
 
-public class Main {
+class Main {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
+
         int n = sc.nextInt();
         sc.nextLine();
 
+        String simonLine = "Simon says";
+        StringBuilder sb = new StringBuilder();
         while (n-- > 0) {
             String line = sc.nextLine();
-            if (line.length() >= 10 && line.substring(0, 10).equals("Simon says")) {
-                String[] words = line.split(" ");
-                System.out.print(" ");
-                for (int i = 2; i < words.length; i++) {
-                    if (i == words.length - 1) {
-                        System.out.print(words[i]);
-                    } else {
-                        System.out.print(words[i] + " ");
-                    }
-                }
-                System.out.println();
+
+            if (line.startsWith(simonLine)) {
+                sb.append(line.substring(simonLine.length(), line.length()) + "\n");
             }
         }
+
+        System.out.print(sb);
+
     }
 }
