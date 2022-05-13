97d9cf098d173bfd01f6c4878d5385ab5fc037c8
Jesse Nguyen <jesse_nguyen9@yahoo.com>
2022-05-13 17:08:36 +1000

    aboveaverage

--- java/aboveaverage/Main.java
+++ java/aboveaverage/Main.java
@@ -1,21 +1,31 @@
-import java.util.Scanner;
+import java.util.*;
 
-class Main {
+public class Main {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int c = sc.nextInt();
 
-        for (int i = 0; i < c; i++) {
+        while (c-- > 0) {
             int n = sc.nextInt();
-            int[] students = new int[];
-        }
 
-            int totalScore = 0;
-            for (int j = 0; j < n; j++) {
-                totalScore += sc.nextInt();
+            int[] arr = new int[n];
+            int total = 0;
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+                total += arr[i];
+            }
+            double average = total / (double) n;
+
+            int numAboveAverage = 0;
+            for (int i = 0; i < n; i++) {
+                if (arr[i] > average) {
+                    numAboveAverage++;
+                }
             }
 
-            System.out.printf("%.3f" + "%", (float) totalScore / (float) n);
+            System.out.printf("%.3f", (numAboveAverage / (double) n) * 100);
+            System.out.println("%");
+        }
 
     }
 }
